516ad6945c52eb5e20436fbdaaee42bbc21b3277
Victor Taiwo <victortaiwo52@gmail.com>
2024-10-23 23:43:52 +0100

    final

--- random_forest.py
+++ random_forest.py
@@ -1,9 +1,34 @@
-stop = 3
-go = 0
+import pandas as pd
+import os
+from sklearn.ensemble import RandomForestRegressor
+#from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
-for i in range(stop):
-    go += 1
-a, b = 1, 2 
-if a == b:
-    c = 0.2885
-    d = 3.233
+# Load the data, and separate the target
+iowa_file_path = '/Users/veek/Predicting-House-Prices---Regression-Techniques/train.csv'
+df = pd.read_csv(iowa_file_path)
+df.drop('Id', axis=1, inplace=True)
+y = df['SalePrice']
+
+# Select string-type features and one-hot encode 
+string_df = df.select_dtypes(include=['object'])
+one_hot = pd.get_dummies(df, columns=string_df.columns)
+
+# Convert one-hot encoded features to int
+for feature in one_hot.columns:
+    if one_hot[feature].dtype == bool:
+        one_hot[feature] = one_hot[feature].astype(int) 
+
+# "cleaned" data
+X = one_hot.drop(columns=['SalePrice'])
+
+# Split into training and validation data
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.25, random_state=1)
+
+# Define a random forest model
+rf_model = RandomForestRegressor(random_state=1)
+rf_model.fit(X_train, y_train)
+rf_val_predictions = rf_model.predict(X_val)
+rf_val_mae = mean_absolute_error(rf_val_predictions, y_val)
+print(rf_val_mae)
